8345747acc5f45989e2c3aa0fb9dc4b6524a5f2d
jalywang123 <jalywangdplove@gamil.com>
2018-06-15 10:30:39 +0800

    esdecededdddeeeeddddeeeedddeeee

--- main.cpp
+++ main.cpp
@@ -18,3 +18,23 @@ int main()
 
     }
 }
+#include <QCoreApplication>
+
+int main()
+{
+    int i,a;
+    for(i = 1; i < 10; i ++)
+    {
+        if(i>3)
+        {
+                if(i%2 == 0)
+                {
+
+                    printf("i = %d\n",i);
+                    break;
+                }
+        }
+
+
+    }
+}
